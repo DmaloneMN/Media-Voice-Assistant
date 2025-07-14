55b56c10a633325feec5a381a4e5ec8bb022b15d
DmaloneMN <87717617+DmaloneMN@users.noreply.github.com>
2025-07-14 15:13:47 -0500

    Update main.tf

--- infrastructure/modules/kubernetes/main.tf
+++ infrastructure/modules/kubernetes/main.tf
@@ -1,18 +1,31 @@
-# In infrastructure/modules/kubernetes/main.tf
 resource "azurerm_kubernetes_cluster" "main" {
   name                = var.cluster_name
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = "${var.cluster_name}-dns"
+  kubernetes_version  = var.kubernetes_version
 
   default_node_pool {
     name       = "default"
     node_count = var.node_count
-    vm_size    = "Standard_D2_v2"
+    vm_size    = var.vm_size
   }
 
-  service_principal {
-    client_id     = var.client_id
-    client_secret = var.client_secret
+  # Choose ONE of these identity options (not both):
+
+  # Option 1: System-assigned managed identity (recommended)
+  identity {
+    type = "SystemAssigned"
+  }
+
+  # Option 2: Service principal (only use if you have specific requirements)
+  # service_principal {
+  #   client_id     = var.client_id
+  #   client_secret = var.client_secret
+  # }
+
+  network_profile {
+    network_plugin = "kubenet"
+    network_policy = "calico"
   }
 }
