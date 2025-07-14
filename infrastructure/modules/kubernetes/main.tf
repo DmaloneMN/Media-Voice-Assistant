8b9438dfb0853392e88b921010118bcd9f8eaf51
DmaloneMN <87717617+DmaloneMN@users.noreply.github.com>
2025-07-14 13:51:47 -0500

    Update main.tf

--- infrastructure/modules/kubernetes/main.tf
+++ infrastructure/modules/kubernetes/main.tf
@@ -1,10 +1,22 @@
 # In infrastructure/modules/kubernetes/main.tf
-resource "azurerm_kubernetes_cluster_node_pool" "user_pool" {
-  name                  = "userpool"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.main.id
-  vm_size               = "Standard_DS3_v2"
-  node_count            = var.node_count
-  enable_auto_scaling   = true
-  min_count            = 2
-  max_count            = 10
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "${var.cluster_name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_D2_v2"
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
