a248c376e9215dee9966238cb9dc9820bcbf85bb
DmaloneMN <87717617+DmaloneMN@users.noreply.github.com>
2025-07-14 15:13:14 -0500

    Update variables.tf

--- infrastructure/modules/kubernetes/variables.tf
+++ infrastructure/modules/kubernetes/variables.tf
@@ -3,26 +3,14 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "location" {
-  description = "Azure region"
-  type        = string
-}
-
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
 }
 
-variable "client_id" {
-  description = "Service principal client ID"
+variable "location" {
+  description = "Azure region"
   type        = string
-  sensitive   = true
-}
-
-variable "client_secret" {
-  description = "Service principal client secret"
-  type        = string
-  sensitive   = true
 }
 
 variable "node_count" {
@@ -30,3 +18,28 @@ variable "node_count" {
   type        = number
   default     = 2
 }
+
+variable "vm_size" {
+  description = "VM size for nodes"
+  type        = string
+  default     = "Standard_D2_v2"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.26"
+}
+
+# Only needed if using service principal
+# variable "client_id" {
+#   description = "Service principal client ID"
+#   type        = string
+#   sensitive   = true
+# }
+
+# variable "client_secret" {
+#   description = "Service principal client secret"
+#   type        = string
+#   sensitive   = true
+# }
