f66b1018255c09810d110e8e91cd7f31d17eeb31
DmaloneMN <87717617+DmaloneMN@users.noreply.github.com>
2025-07-14 13:50:53 -0500

    Create variables.tf

---
+++ infrastructure/modules/kubernetes/variables.tf
@@ -0,0 +1,32 @@
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Service principal client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "Service principal client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "node_count" {
+  description = "Number of AKS nodes"
+  type        = number
+  default     = 2
+}
