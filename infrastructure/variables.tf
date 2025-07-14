db5b5192dbd9d0e8023cceefbe923b1a625d59e0
DmaloneMN <87717617+DmaloneMN@users.noreply.github.com>
2025-07-14 15:17:30 -0500

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,3 @@
-variable "client_id" {
-  description = "Azure service principal client ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_secret" {
-  description = "Azure service principal client secret"
-  type        = string
-  sensitive   = true
-}
-
 variable "environment" {
   description = "Deployment environment"
   type        = string
